8aa196727cd7fd8d3a06bbe8f7c27b47e2b180e7
akarapukranthi <akarapukranthi444@gmail.com>
2025-10-15 16:55:30 +0530

    basic code

---
+++ basic_app/src/App.jsx
@@ -0,0 +1,13 @@
+function code () {
+
+  return <div class="root">
+    <center>
+      <h1>JavaScript</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ducimus? Provident nesciunt, quidem iure ipsum delectus corrupti temporibus fugit rerum voluptatibus, nostrum minima. Enim nam delectus praesentium cum omnis inventore?
+    Distinctio ad earum tenetur sint maxime id. Doloribus ab soluta repellendus tenetur modi recusandae reiciendis expedita eveniet? Deleniti dolor consectetur excepturi assumenda quia voluptatum, voluptates veritatis. Tempore exercitationem ipsam eveniet?</p>
+
+    </center>
+  </div>
+}
+
+export default code 
---
+++ react_app/src/App.jsx
@@ -0,0 +1,10 @@
+function react() {
+
+  return <div>
+    <h1>react app</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci nostrum harum dolor sunt dolorum numquam
+       exercitationem quidem corporis, expedita culpa quibusdam id quod pariatur nulla perferendis sint illum beatae cumque!</p>
+  </div>
+}
+
+export default react
